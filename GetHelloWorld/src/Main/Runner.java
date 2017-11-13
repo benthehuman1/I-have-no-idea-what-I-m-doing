330a3a37ff96d9a0a98be2e61dbdf972bb8ca663
ben <ben@192.168.1.228>
2017-11-12 22:08:29 -0600

    Here goes nothing

--- GetHelloWorld/src/Main/Runner.java
+++ GetHelloWorld/src/Main/Runner.java
@@ -4,5 +4,6 @@ public class Runner {
 	public static void main(String[] args){
 		System.out.println("Hello GitHub!");
 		System.out.println("Hello GitHub!");
+		System.out.println("This is from my mac!");
 	}
 }
